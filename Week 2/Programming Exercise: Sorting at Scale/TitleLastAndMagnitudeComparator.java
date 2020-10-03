734e6635c1f9a3c7e5cd08ad1ca7feb87c6358b8
Omkar Dutta <omkar1620@gmail.com>
2020-10-03 15:46:30 +0530

    Add files via upload

---
+++ Week 2/Programming Exercise: Sorting at Scale/TitleLastAndMagnitudeComparator.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of TitleLastAndMagnitudeComparator here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class TitleLastAndMagnitudeComparator implements Comparator<QuakeEntry>
+{
+    public int compare(QuakeEntry q1, QuakeEntry q2) 
+    {
+        String lastWord1 = q1.getInfo().substring(q1.getInfo().lastIndexOf(" ")+1);
+        String lastWord2 = q2.getInfo().substring(q2.getInfo().lastIndexOf(" ")+1);
+        if(lastWord1.compareTo(lastWord2)==0)
+        {
+            return Double.compare(q1.getMagnitude(), q2.getMagnitude());
+        }
+        else
+            return lastWord1.compareTo(lastWord2);
+    }
+}
